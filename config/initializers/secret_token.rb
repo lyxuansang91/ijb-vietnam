f60c2d05ae5c511a61a67cf64bd6744fecbe30d2
lyxuansang1991 <lyxuansang1991@gmail.com>
2012-10-28 23:56:11 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ijbvn::Application.config.secret_token = '788a6ee9d746b8869470f38cac28960f30938a61a54394319dbaeed74069238f7d1bdfadb48bafe95e8aac4a98554280b101e2a4e02199a1cef82f2e4f87f9de'
